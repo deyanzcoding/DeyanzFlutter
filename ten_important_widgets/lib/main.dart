99d4f52db27030d698785231edefcaa3c658b2da
Deyanz Coding <deyanzcoding@gmail.com>
2025-08-22 02:24:34 -0700

    important widget
    today i learn important widgets like container, expanded widget, stack, CircleAvatar, RichText..

--- ten_important_widgets/lib/main.dart
+++ ten_important_widgets/lib/main.dart
@@ -1,122 +1,274 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          // backgroundColor: Colors.red.shade200,
+          title: Center(
+            child: Text(
+              'Ten Importand Widgets',
+              style: TextStyle(fontSize: 20, color: Colors.black),
             ),
-          ],
+          ),
         ),
+
+          body:
+
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+//[1, 2] --Container & Expanded widget.
+//  Column(
+//   mainAxisAlignment: MainAxisAlignment.center,
+//   children: [
+//     Center(
+//       child: Container(
+//         width: 100,
+//         height: 100,
+//         decoration: BoxDecoration(
+//           color: Colors.orange,
+//           border: Border.all(width: 2, color: Colors.black),
+//           borderRadius: BorderRadius.only(
+//             topLeft: Radius.circular(40),
+//             bottomRight: Radius.circular(40),
+//             topRight: Radius.circular(10),
+//             bottomLeft: Radius.circular(10),
+//           ),
+//         ),
+//         child: Center(
+//           child: Text(
+//             'Container 1\n.only',
+//             style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+//           ),
+//         ),
+//       ),
+//     ),
+//
+//     SizedBox(width: 10),
+//
+//     Container(
+//       width: 120,
+//       height: 120,
+//       alignment: Alignment.bottomLeft,
+//       transform: Matrix4.rotationZ(0.1),
+//       padding: EdgeInsets.all(10),
+//       margin: EdgeInsets.all(10),
+//
+//       decoration: BoxDecoration(
+//         color: Colors.orange,
+//         borderRadius: BorderRadius.circular(20),
+//         boxShadow: [BoxShadow(color: Colors.orange, blurRadius: 100)],
+//         image: DecorationImage(
+//           image: NetworkImage(
+//             'https://www.peanutsquare.com/wp-content/uploads/2023/06/Advanced-Flutter-Widgets-Exploring-Custom-and-Composite-Widgets-jpg.webp',
+//           ),
+//           fit: BoxFit.cover,
+//         ),
+//       ),
+//       child: Text(
+//         'Container 2\n.circular',
+//         style: TextStyle(fontSize: 10, fontWeight: FontWeight.w600),
+//       ),
+//     ),
+//
+//     SizedBox(width: 10),
+//
+//     Center(
+//       child: Container(
+//         width: 100,
+//         height: 100,
+//         decoration: BoxDecoration(
+//           color: Colors.orange,
+//           borderRadius: BorderRadius.only(
+//             topRight: Radius.circular(40),
+//             topLeft: Radius.circular(10),
+//             bottomRight: Radius.circular(10),
+//             bottomLeft: Radius.circular(40),
+//           ),
+//           border: Border.all(color: Colors.black, width: 2),
+//         ),
+//         child: Center(
+//           child: Text(
+//             'Container 1\nborder.all',
+//             style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+//           ),
+//         ),
+//       ),
+//     ),
+//
+//     SizedBox(height: 20),
+//
+//     Container(
+//       width: 150,
+//       height: 60,
+//       decoration: BoxDecoration(
+//         color: Colors.red,
+//         borderRadius: BorderRadius.circular(15),
+//         border: Border.all(color: Colors.black, width: 3),
+//         boxShadow: [BoxShadow(color: Colors.green, blurRadius: 30)],
+//       ),
+//       child: Center(
+//         child: Text('Login', style: TextStyle(fontSize: 20)),
+//       ),
+//     ),
+//
+//     Expanded(
+//         flex: 1,
+//         child: Container(
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(colors: [Colors.green, Colors.yellow], begin: Alignment.topLeft, end: Alignment.bottomRight),
+//           shape: BoxShape.rectangle,
+//         ),
+//         child: Text('Expanded Widget')
+//     )
+//                  ),
+//
+//     Expanded(
+//       flex: 2,
+//       child: Container(
+//           color: Colors.green,
+//           child: Text('Not-Expanded Widget')
+//       ),
+//     ),
+//   ],
+// ),
+
+
+// [3] --Expanded Widget
+// Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// SizedBox(
+// child: Stack(
+// alignment: Alignment.center,
+//
+// children: [
+// Container(
+// height: 150,
+// width: 150,
+// color: Colors.red,
+// ),
+// Positioned(
+// bottom: 0,
+// right: 30,
+// child: Container(
+// height: 70,
+// width: 70,
+// color: Colors.green,
+// ),
+// ),
+//
+// Container(
+// height: 30,
+// width: 30,
+// color: Colors.blue,
+// ),
+// Positioned(
+// top: 0,
+// left: 50,
+//
+// child: Text('Deyan'),
+// ),
+// ],
+// ),
+// ),
+//
+// SizedBox(height: 25,),
+//
+// Stack(
+// children: [
+// Container(
+// height: 150,
+// width: 150,
+// color: Colors.red,
+// ),
+// Container(
+// height: 100,
+// width: 100,
+// color: Colors.orange,
+// ),
+// Container(
+// height: 50,
+// width: 50,
+// color: Colors.green,
+// ),
+// ],
+// )
+// ],
+// )
+
+
+//[4] --Divider, VerticalDivider
+// Divider(
+// color: Colors.black,
+// thickness: 2.5,
+// ),
+// SizedBox(
+// height: 50,
+// child: VerticalDivider(
+// color: Colors.pink,
+// thickness: 2.5,
+//
+// ),
+// ),
+// Divider(
+// color: Colors.black,
+// thickness: 2.5,
+// ),
+
+
+// [5] --CircleAvatar
+// Column(
+// mainAxisAlignment: MainAxisAlignment.start,
+// children: [
+// SizedBox(
+// height: 100,    //use for column
+// width: 100,     //use for row
+// ),
+//
+//
+// Center(
+// child: CircleAvatar(
+// radius: 100,
+// backgroundColor: Colors.grey,
+// foregroundColor: Colors.red,
+//
+// backgroundImage: NetworkImage('https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg'),
+// // child: Icon(Icons.person),
+// // child: Image(image: AssetImage('/assets/images/deyan.png'),),
+// // child: Text('D', style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),),
+// ),
+// )
+// ],
+// )
+
+
+// [6] --RichText
+// Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// Center(
+// child: RichText(text: TextSpan(text: 'Don\'t have any account?   ',
+// style: Theme.of(context).textTheme.bodyMedium,
+// children: [
+// TextSpan(text: 'Sign Up, ', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24)),
+// ]
+// ),
+// ),
+// )
+// ],
+// )
