2b4b037b552f3c5a2cc7ffa9343dcbbc70a97e17
Deyanz Coding <deyanzcoding@gmail.com>
2025-08-25 09:39:48 -0700

    badges practice

--- pub_dot_dev/lib/main.dart
+++ pub_dot_dev/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart' as badges;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -28,7 +29,11 @@ class _MyAppState extends State<MyApp> {
                 'Lorem Ipsum : ',
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
+
+              //using custom icons using pub.dev website
               Icon(FontAwesomeIcons.house),
+              Icon(CupertinoIcons.add),
+              Icon(Icons.add),
               ReadMoreText(
                 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
                 trimMode: TrimMode.Line,
@@ -36,8 +41,36 @@ class _MyAppState extends State<MyApp> {
                 trimCollapsedText: 'Show all ...',
                 trimExpandedText: 'Hide detail',
                 moreStyle: TextStyle(color: Colors.red, fontSize: 10),
+                lessStyle: TextStyle(color: Colors.red, fontSize: 10),
                 style: TextStyle(color: Colors.black38),
               ),
+              badges.Badge(
+                badgeContent: Icon(Icons.check, color: Colors.white, size: 50, ),
+                badgeAnimation: badges.BadgeAnimation.rotation(
+                    animationDuration: Duration(seconds: 1),
+                    colorChangeAnimationDuration: Duration(seconds: 1),
+                  loopAnimation: false,
+                  curve: Curves.fastOutSlowIn,
+                  colorChangeAnimationCurve: Curves.easeInCubic,
+                ),
+                badgeStyle: badges.BadgeStyle(
+                  shape: badges.BadgeShape.instagram,
+                  badgeColor: Colors.blue,
+                  borderSide: BorderSide(color: Colors.black, width: 5),
+                  borderGradient: badges.BadgeGradient.linear(
+                    colors: [Colors.red, Colors.yellow],
+                    begin: Alignment.topLeft,
+                    end: Alignment.topRight,
+                  ),
+                  elevation: 0,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50,),
+                  child: Text('Badge', style: TextStyle(color: Colors.blue, fontSize: 30),),
+                ),
+              ),
+
+
             ],
           ),
         ),
