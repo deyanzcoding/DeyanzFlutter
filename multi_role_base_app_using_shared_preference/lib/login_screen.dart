fa453804e3a3bb04c46e4b8508d4b421fef4baac
Deyanz Coding <deyanzcoding@gmail.com>
2025-08-27 10:36:56 -0700

    adding shared preference

--- multi_role_base_app_using_shared_preference/lib/login_screen.dart
+++ multi_role_base_app_using_shared_preference/lib/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:multi_role_base_app_using_shared_preference/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,6 +10,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final ageController = TextEditingController();
+  final isLogin = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,23 +23,48 @@ class _LoginScreenState extends State<LoginScreen> {
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment  .center,
           children: [
-            Image(image: NetworkImage('https://images.pexels.com/photos/14408339/pexels-photo-14408339.jpeg')),
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: 'Email',
+              ),
+            ),
+              TextFormField(
+                controller: passwordController,
+              decoration: InputDecoration(
+                hintText: 'Password ',
+              ),
+            ),
+            TextFormField(
+                controller: ageController,
+                keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: 'Age ',
+              ),
+            ),
+            SizedBox(height: 10,),
+            // Image(image: NetworkImage('https://images.pexels.com/photos/14408339/pexels-photo-14408339.jpeg')),
             SizedBox(
               height: 10,
             ),
 
             InkWell(
-              onTap: () {
-                asifTaj();
+              onTap: () async {
+                SharedPreferences sp = await SharedPreferences.getInstance();
+                sp.setString('email', emailController.text.toString());
+                sp.setString('age', ageController.text.toString());
+                
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()));
               },
               child: Container(
                 height: 50,
                 width: double.infinity,
                 color: Colors.grey,
                 child: Center(
-                  child: Text('Explore'),
+                  child: Text('Save details'),
                 ),
               ),
             )
