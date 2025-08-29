5a3a966cb88dfd1a935de64798f4574436ed5fa0
Deyanz Coding <deyanzcoding@gmail.com>
2025-08-29 09:45:12 -0700

    creating home_screen

---
+++ navigation_routing_chp_11/lib/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            child: Center(
+              child: Text('Screen 1'),
+            ),
+          )
+        ]
+      ),
+    );
+  }
+}
