0080573d84e031ca49a3159e9f308faaaa9f7d4f
deyanzcoding <deyanzcoding@gmail.com>
2025-08-31 09:03:03 +0500

    adding password hide/show feature

--- login_page/lib/login_screen.dart
+++ login_page/lib/login_screen.dart
@@ -13,20 +13,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _useremailController = TextEditingController();
-
-  Future<void> _login() async {
-    final prefs = await SharedPreferences.getInstance();
-
-    await prefs.setString('username', _usernameController.text);
-    await prefs.setString('useremail', _useremailController.text);
-    await prefs.setBool('isLoggedIn', true);
-
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => HomeScreen())
-    );
-  }
+  //A boolean variable to manage the password's visibility state.
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -35,163 +23,178 @@ class _LoginScreenState extends State<LoginScreen> {
       home: Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: Column(
-            children: [
-              // Logo
-              SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image(
-                    image: AssetImage('assets/images/logo.png'),
-                    height: 70,
-                    width: 70,
-                  ),
-                  SizedBox(width: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Maintenance',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontFamily: 'Rubik Medium',
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // Logo
+                SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/logo.png'),
+                      height: 70,
+                      width: 70,
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Maintenance',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontFamily: 'Rubik Medium',
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Box',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontFamily: 'Rubik Medium',
-                          color: Color(0xfff9703B),
+                        Text(
+                          'Box',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontFamily: 'Rubik Medium',
+                            color: Color(0xfff9703B),
+                          ),
+                          textAlign: TextAlign.left,
                         ),
-                        textAlign: TextAlign.left,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 100),
-              // After-logo content
-              Center(
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontFamily: 'Rubik Medium',
-                    color: Color(0xff2D3142),
-                  ),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 10),
-              Center(
-                child: Text(
-                  'Welcome back! We\'ve missed you 😊',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontFamily: 'Rubik Regular',
-                    color: Color(0xff4c5980),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
+                SizedBox(height: 100),
 
-              //TextFormField (inputbox for email & password)
-              //email
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextFormField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                    hintText: "Email",
-                    prefixIcon: Icon(
-                      Icons.alternate_email,
-                      color: Color(0xff323f4B),
-                    ),
-                    fillColor: Color(0xffF8F9FA),
-                    filled: true,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                      borderRadius: BorderRadius.circular(20),
+                // After-logo content
+                Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: 'Rubik Medium',
+                      color: Color(0xff2D3142),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-
-              //password
-              Padding(
-                padding: const EdgeInsets.only(right: 20, left: 20),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    prefixIcon: Icon(Icons.lock_open, color: Color(0xff323f4B)),
-                    suffixIcon: Icon(Icons.visibility_off_outlined),
-                    fillColor: Color(0xffF8F9FA),
-                    filled: true,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 15),
-
-              // Box Container
-              InkWell(
-                onTap: () {
-                  _login();
-                  Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => HomeScreen())
-                  );
-                },
-                child: MyButton(text: 'LOGIN'),
-              ),
-
-              SizedBox(height: 15),
-
-              //row for signup
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Don\'t have an account? ',
+                SizedBox(height: 10),
+                Center(
+                  child: Text(
+                    'Welcome back! We\'ve missed you 😊',
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: 16,
-                      color: Color(0xff4C5980),
-                      fontFamily: 'Rubik Medium',
+                      fontFamily: 'Rubik Regular',
+                      color: Color(0xff4c5980),
                     ),
                   ),
-                  InkWell(
-                    onTap: () {
-                      // Navigate to the signup screen
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => SignupScreen())
-                      );
-                    },
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: 'Rubik Medium',
-                        color: Color(0xffF9703B),
+                ),
+                SizedBox(height: 20),
+
+                //TextFormField (inputbox for email & password)
+                //email
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                      prefixIcon: Icon(
+                        Icons.alternate_email,
+                        color: Color(0xff323f4B),
+                      ),
+                      fillColor: Color(0xffF8F9FA),
+                      filled: true,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(20),
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(height: 10),
+
+                //password
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: TextFormField(
+                    obscureText: !_isPasswordVisible,
+                    obscuringCharacter: '*',
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      prefixIcon: Icon(Icons.lock_open, color: Color(0xff323f4B)),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          // Changes the icon based on the state.
+                          _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          // Call setState to rebuild the widget and toggle the visibility state.
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
+                      ),
+
+                      fillColor: Color(0xffF8F9FA),
+                      filled: true,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+
+                // Box Container
+                InkWell(
+                  onTap: () {
+                    Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => HomeScreen())
+                    );
+                  },
+                  child: MyButton(text: 'LOGIN'),
+                ),
+
+                SizedBox(height: 15),
+
+                //row for signup
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account? ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xff4C5980),
+                        fontFamily: 'Rubik Medium',
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        // Navigate to the signup screen
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => SignupScreen())
+                        );
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Rubik Medium',
+                          color: Color(0xffF9703B),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- login_page/lib/signup_screen.dart
+++ login_page/lib/signup_screen.dart
@@ -15,165 +15,178 @@ class _SignupScreenState extends State<SignupScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 100),
-                child: Container(
-                  height: 80,
-                  width: 80,
-                  decoration: BoxDecoration(
-                    color: Color(0xffF9703B),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Icon(Icons.person, size: 80, color: Colors.white),
-                ),
-              ),
-            ),
-
-            SizedBox(height: 10),
-            // After-logo content
-            Center(
-              child: Text(
-                'Create Account',
-                style: TextStyle(
-                  fontSize: 26,
-                  fontFamily: 'Rubik Medium',
-                  color: Color(0xff2D3142),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 25),
-              child: Text(
-                "Sign up to get started 🚀",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'Rubik Regular',
-                  color: Color(0xff4c5980),
-                ),
-              ),
-            ),
-
-            SizedBox(height: 10),
-
-            //name TextFormField
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                // controller: _usernameController,
-                decoration: InputDecoration(
-                  hintText: "Name",
-                  prefixIcon: Icon(Icons.person, color: Color(0xff323f4B)),
-                  fillColor: Color(0xffF8F9FA),
-                  filled: true,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                    borderRadius: BorderRadius.circular(20),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Color(0xffF9703B),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Icon(Icons.person, size: 80, color: Colors.white),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-
-            //email TextFormField
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  prefixIcon: Icon(Icons.email),
-                  fillColor: Color(0xffF8F9FA),
-                  filled: true,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                    borderRadius: BorderRadius.circular(20),
+          
+              SizedBox(height: 10),
+              // After-logo content
+              Center(
+                child: Text(
+                  'Create Account',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontFamily: 'Rubik Medium',
+                    color: Color(0xff2D3142),
                   ),
                 ),
               ),
-            ),
-
-            SizedBox(height: 10),
-
-            //password TextFormField
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  prefixIcon: Icon(Icons.lock),
-                  fillColor: Color(0xffF8F9FA),
-                  filled: true,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                    borderRadius: BorderRadius.circular(20),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 25),
+                child: Text(
+                  "Sign up to get started 🚀",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Rubik Regular',
+                    color: Color(0xff4c5980),
                   ),
                 ),
               ),
-            ),
-
-            SizedBox(height: 10),
-
-            //confirm password TextFormField
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Confirm Password',
-                  prefixIcon: Icon(Icons.lock),
-                  fillColor: Color(0xffF8F9FA),
-                  filled: true,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffE4E7EB)),
-                    borderRadius: BorderRadius.circular(20),
+              SizedBox(height: 10),
+          
+              //name TextFormField
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  // controller: _usernameController,
+                  decoration: InputDecoration(
+                    hintText: "Name",
+                    prefixIcon: Icon(Icons.person, color: Color(0xff323f4B)),
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
                 ),
               ),
-            ),
-
-            SizedBox(height: 10),
-
-            //signup button
-            MyButton(text: 'SIGN UP',),
-
-            SizedBox(height: 8,),
-
-            InkWell(
-              onTap: () {
-                Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()),
-                );
-              },
-              child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 10),
+          
+              //email TextFormField
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    prefixIcon: Icon(Icons.email),
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+          
+              //password TextFormField
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    prefixIcon: Icon(Icons.lock),
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+          
+              //confirm password TextFormField
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Password',
+                    prefixIcon: Icon(Icons.lock),
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+          
+              //signup button
+              MyButton(text: 'SIGN UP'),
+              SizedBox(height: 8),
+          
+              //already have account
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Already have an account? ', style: TextStyle(color: AppColors.appThemeGrey, fontSize: 16, fontFamily: 'Rubik Medium',)),
-                  Text('Login', style: TextStyle(fontSize: 16, color: AppColors.appThemeBold, fontFamily: 'Rubik Medium'),),
+                  Text(
+                    'Already have an account? ',
+                    style: TextStyle(
+                      color: AppColors.appThemeGrey,
+                      fontSize: 16,
+                      fontFamily: 'Rubik Medium',
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: AppColors.appThemeBold,
+                        fontFamily: 'Rubik Medium',
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
