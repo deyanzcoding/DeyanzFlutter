b90823ed82fa663031f2a07a1b6b29b93f33a28f
deyanzcoding <deyanzcoding@gmail.com>
2025-09-01 08:40:03 +0500

    Update home_screen.dart
    adding more student data like:
    Department,
    Discipline,
    Semester,
    Status.

--- login_page/lib/home_screen.dart
+++ login_page/lib/home_screen.dart
@@ -46,13 +46,15 @@ class _HomeScreenState extends State<HomeScreen> {
         foregroundColor: Colors.white,
       ),
       body: SafeArea(
-            top: true,
+        top: true,
         child: SingleChildScrollView(
           child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(height: 50),
+
+              //logo & uni name
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -60,7 +62,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     'assets/images/logo.png',
                     height: 60,
                     width: 60,
-                    errorBuilder: (context, error, stackTrace) => const Icon(Icons.error, size: 70),
+                    errorBuilder: (context, error, stackTrace) =>
+                        const Icon(Icons.error, size: 70),
                   ),
                   const SizedBox(width: 10),
                   const Column(
@@ -90,49 +93,258 @@ class _HomeScreenState extends State<HomeScreen> {
               const SizedBox(height: 18),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 20),
+
+                //student card
                 child: Container(
-                  height: 380,
+                  height: 400,
                   width: double.infinity,
                   decoration: BoxDecoration(
-                      color: Colors.grey,
+                    color: Color(0xff_71_00_00),
                     borderRadius: BorderRadius.circular(30),
                     boxShadow: [
                       BoxShadow(
-                          color: Colors.grey.shade300,
-                          spreadRadius: 5,
-                          blurRadius: 7,
+                        color: Colors.grey.shade300,
+                        spreadRadius: 5,
+                        blurRadius: 7,
                       ),
-                    ]
-
+                    ],
                   ),
                   child: Column(
                     children: [
                       const SizedBox(height: 15),
                       Container(
-                        height: 80,
-                        width: 80,
+                        height: 90,
+                        width: 90,
                         decoration: BoxDecoration(
                           color: Colors.white,
                           shape: BoxShape.circle,
-                          image: DecorationImage(image: AssetImage('assets/images/deyan_white.png'),
-                          fit: BoxFit.cover,
-                          )
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/deyan_white.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
 
+                      Text(
+                        username.toUpperCase(),
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900,
+                          fontFamily: 'Rubik Medium',
+                        ),
+                      ),
+                      Text(
+                        's/o Khursheed Ahmed',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontFamily: 'Rubik Regular',
+                        ),
+                      ),
+                      Text(
+                        'AWKUM-231008077',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontFamily: 'Rubik Regular',
+                        ),
                       ),
 
-                      Text('$username', style: TextStyle(fontSize: 22, color: Colors.white, fontWeight: FontWeight.w900, fontFamily: 'Rubik Medium'),),
-                      Text('s/o Khursheed Ahmed', style: TextStyle(fontSize: 15, color: Colors.white, fontFamily: 'Rubik Regular'),),
-                      Text('AWKUM-231008077', style: TextStyle(fontSize: 15, color: Colors.white, fontFamily: 'Rubik Regular'),),
+                      //Campus
+                      SizedBox(height: 20),
+                      Divider(height: 1, color: Colors.black),
+                      SizedBox(height: 5),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1, // Label takes 1 part of the space
+                                  child: Text(
+                                    'Campus',
+                                    style: const TextStyle(
+                                      fontSize: 15.5,
+                                      color: Colors.white,
+                                      fontFamily: 'Rubik Regular',
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 2, // Value takes 2 parts of the space
+                                  child: Text(
+                                    'Garden',
+                                    style: const TextStyle(
+                                      fontSize: 15.5,
+                                      color: Colors.white,
+                                      fontFamily: 'Rubik Regular',
+                                    ),
+                                    textAlign: TextAlign.right,
+                                  ),
+                                ),
 
-                     SizedBox(height: 10,),
-                      Divider(height: 1, color: AppColors.appThemeGrey,),
+                                                    ],
+                                                  ),
+                          ),
+                      SizedBox(height: 5),
+                      Divider(height: 1, color: Colors.black),
 
-                      Row(children: [
-                        SizedBox(width: 20,),
-                        Text('Campus', )
-                      ],)
+                      //department
+                      SizedBox(height: 5),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Text(
+                                'Department',
+                                style: TextStyle(
+                                  fontSize: 15.5,
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik Regular',
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
 
+                            Expanded(
+                              flex: 2,
+                              child: Text(
+                                'Computer Science',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik Regular',
+                                ),
+                                textAlign: TextAlign.right,
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Divider(height: 1, color: Colors.black),
+
+                      //discipline
+                      SizedBox(height: 5),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Text(
+                                'Discipline',
+                                style: TextStyle(
+                                  fontSize: 15.5,
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik Regular',
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+
+                            Expanded(
+                              flex: 2,
+                              child: Text(
+                                'BSCS-Software',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik Regular',
+                                ),
+                                textAlign: TextAlign.right,
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Divider(height: 1, color: Colors.black),
+
+                      //semister
+                      SizedBox(height: 5),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Text(
+                                'Semester',
+                                style: TextStyle(
+                                  fontSize: 15.5,
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik Regular',
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+
+                            Expanded(
+                              flex: 2,
+                              child: Text(
+                                '5th - (section: A)',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik Regular',
+                                ),
+                                textAlign: TextAlign.right,
+                              ),
+                            ),
+
+                            //status
+
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Divider(height: 1, color: Colors.black),
+
+                      //status
+                      SizedBox(height: 5),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Text(
+                                'Status',
+                                style: TextStyle(
+                                  fontSize: 15.5,
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik Regular',
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+
+                            Expanded(
+                              flex: 2,
+                              child: Text(
+                                'InProgress',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik Regular',
+                                ),
+                                textAlign: TextAlign.right,
+                              ),
+                            ),
+
+                            //status
+
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Divider(height: 1, color: Colors.black),
                     ],
                   ),
                 ),
