493759a723e90bb102e51fa323187dc4bd8b9152
deyanzcoding <deyanzcoding@gmail.com>
2025-09-06 07:41:31 +0500

    create simple model

--- api_practice/lib/home_screen.dart
+++ api_practice/lib/home_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'PostsModel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +13,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  List<PostsModel> postList = [];
+
+  Future<List<PostsModel>> getPostApi() async {
+    final response = await http.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(1));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,11 +38,20 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('API practice'),
       ),
       body: Column(
-        children: [
-
-        ],
-      ),
-    );
+          children: [
+          FutureBuilder(
+          future: getPostApi(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading');
+        } else {
+          return ListView.builder(itemBuilder: (context, index) {
+            return Text(index.toString());
+          });
+        }
+      },
+          )],
+    ),);
   }
 }
 
